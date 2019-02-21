532540e085cdafe145111f1b552b10abbea43ca9
Norton <norton0395@gmail.com>
2019-02-21 21:24:36 +0700

    first commit

---
+++ src/consts.js
@@ -0,0 +1,7 @@
+export const
+  cmdImport = 'gjs-open-import-webpage',
+  cmdSave = 'gjs-call-save',
+  cmdDeviceDesktop = 'set-device-desktop',
+  cmdDeviceTablet = 'set-device-tablet',
+  cmdDeviceMobile = 'set-device-mobile',
+  cmdClear = 'canvas-clear';
